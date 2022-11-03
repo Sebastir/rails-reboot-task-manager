d1c6e4fff48b578139d6a98429de7d64ab397f66
Seb Rojas <src.py@icloud.com>
2022-11-03 16:12:31 +0100

    styling home page

---
+++ app/javascript/controllers/typed.js
@@ -0,0 +1,8 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['creative people', 'organize people'],
+  typeSpeed: 40
+};
+
+var typed = new Typed('.type', options);
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "home#index"
   get "/tasks", to: "tasks#index"
   get "/tasks/new", to: "tasks#new", as: :task_new
   get "/tasks/:id", to: "tasks#show", as: :task
